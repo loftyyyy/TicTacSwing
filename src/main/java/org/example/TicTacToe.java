e064c72d83ea68c62e8686bdad50571992cac1df
loftyyyy <lofty08.business@gmail.com>
2024-06-19 00:26:30 +0800

    lf will be replaced

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -2,6 +2,10 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 public class TicTacToe {
@@ -11,16 +15,25 @@ public class TicTacToe {
 
     }
     public void startGUI(){
-        JFrame window = new JFrame();
-        JPanel buttonPanel = new JPanel();
+        JFrame window = new JFrame("Tic Tac Toe Nigga");
+        JPanel buttonPanel = new JPanel(new GridLayout(3,3));
+        JPanel controlPanel = new JPanel();
 
         for(int i = 0; i < 9;i++){
             JButton button = new JButton();
+//            button.setPreferredSize();
+            button.setFocusPainted(false);
+            button.setFont(new Font("Lato", Font.PLAIN, 160));
+            button.setBackground(Color.decode("#F5E7B2"));
+            button.setHorizontalAlignment(SwingConstants.CENTER);
+            button.setVerticalAlignment(SwingConstants.CENTER);
+            button.addActionListener(new ButtonActionListener());
             buttons.add(button);
             buttonPanel.add(button);
         }
 
         window.getContentPane().add(BorderLayout.CENTER, buttonPanel);
+        window.getContentPane().add(BorderLayout.SOUTH, controlPanel);
 
         window.setSize(500,700);
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -30,4 +43,27 @@ public class TicTacToe {
 
 
     }
+    class ButtonActionListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            for(JButton button : buttons){
+                if(button == e.getSource()){
+                    button.setText("x");
+
+                }
+
+
+            }
+        }
+
+
+    }
+    class MouseActionListener extends MouseAdapter {
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            super.mouseReleased(e);
+            System.out.println("released");
+        }
+    }
+
 }
