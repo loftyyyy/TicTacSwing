39acd860296b23f0845b7be0fca9d63be74f7573
loftyyyy <lofty08.business@gmail.com>
2024-06-22 00:03:45 +0800

    hello

--- src/main/java/org/example/TicTacToeAI.java
+++ src/main/java/org/example/TicTacToeAI.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 
 public class TicTacToeAI {
     //TODO: Create a AIMove and PlayerMove methods
+    // also create a checkWin that takes a board as the argumentt.
 
 
 }
